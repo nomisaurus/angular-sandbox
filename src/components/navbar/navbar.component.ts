65dd4aa46a3bcfc72ce8d0c6a451aa136820f2e4
nomisaurus <mn.ulhaq@gmail.com>
2023-05-03 06:30:17 -0400

    2 files modified

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
@@ -11,4 +12,10 @@ export class NavbarComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  toggleMenu(menuElement: HTMLDivElement) {
+    menuElement.classList.contains('show')
+      ? menuElement.classList.remove('show')
+      : menuElement.classList.add('show');
+  }
 }
